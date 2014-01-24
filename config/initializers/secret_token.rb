fd87cb00b655df12bff840aa2e9d748d62fc9ada
vigneshwarvicky <vigneshwarmca@gmail.com>
2014-01-24 15:33:11 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RbfbeneApp::Application.config.secret_token = 'e80cf9be2496fb8a11950234b2c0fbb449a6f1a91d84c5f527a2d65e80c0187d9456a25e885435631ac04a1cc66b8b7b441defe784169b7b2bd9c17fafbef3e1'
